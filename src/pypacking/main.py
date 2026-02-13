8b43752809fb9de8c339660b32b2dd7fc6d49f67
Relief Melone <relief.melone@gmail.com>
2026-02-13 10:03:12 +0100

    added basicschema

--- src/pypacking/main.py
+++ src/pypacking/main.py
@@ -1,3 +1,11 @@
+import sys, os, json
+
+this_folder = os.path.dirname(__file__)
+schema_folder = os.path.join(this_folder, "..","schemas")
+file = open(f"{schema_folder}/basic.json")
+
+basic_schema = json.loads(file.read())
+
 def hello(name: str) -> None:
     print(f"Hello, {name}!")
 
