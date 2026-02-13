e03bc4b75de1593c6b7a188c9cfddbf9e679fd1c
Relief Melone <relief.melone@gmail.com>
2026-02-13 10:18:12 +0100

    schemas in package directory

--- src/pypacking/main.py
+++ src/pypacking/main.py
@@ -1,7 +1,7 @@
 import sys, os, json
 
 this_folder = os.path.dirname(__file__)
-schema_folder = os.path.join(this_folder, "..","schemas")
+schema_folder = os.path.join(this_folder,"schemas")
 file = open(f"{schema_folder}/basic.json")
 
 basic_schema = json.loads(file.read())
